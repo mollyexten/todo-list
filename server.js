a7db9ca4341cb9f6943673418c76cb9abe01a839
mollyexten <molly.exten@gmail.com>
2021-07-28 11:05:50 -0400

    added scripts to start server.js

--- server.js
+++ server.js
@@ -2,3 +2,19 @@
 // const express = require("express");
 // New syntax (with "type": "module"):
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+const PORT = process.env.PORT || 4567;
+
+// Middleware
+// express.json gives access to req.body
+app.use(express.json());
+app.use(cors());
+// Change morgan after deployment once everything works ('tiny')
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("<h1>Let's get stuff done!</h1>"))
+
+app.listen(PORT, console.log(`Connected to port: ${PORT}`));
